51d73c7b2849fde5c2b71f0ed6f97c74f0b269aa
Nidurshan <nidurshanpro@gmail.com>
2023-09-30 14:18:15 +0530

    Add comments

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-
+// Buttons
 var carBtn = document.getElementById("car-img");
 var bikeBtn = document.getElementById("bike-img");
 var spaceBtn = document.getElementById("space-img");
 var phoneBtn = document.getElementById("phone-img");
 var natureBtn = document.getElementById("nature-img");
 
+// Images
 var carImage = document.getElementById("car-image");
 var bikeImage = document.getElementById("bike-image");
 var spaceImage = document.getElementById("space-image");
 var phoneImage = document.getElementById("phone-image");
 var natureImage = document.getElementById("nature-image");
 
+// Preview Image
 var previewImage = document.getElementById("preview")
 
+// Image and button array
 var images = document.querySelectorAll(".image");
 var btns = document.querySelectorAll(".btn");
 
+// To set preview image
 function setPreviewImage (image) {
     var computedStyle = getComputedStyle(image);
     var backgroundImage = computedStyle.getPropertyValue("background-image");
